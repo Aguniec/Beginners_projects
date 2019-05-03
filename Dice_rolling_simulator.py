e8b07a9aad47cde78450788ac73479e8e36f661c
Aguniec <aguniecc@gmail.com>
2019-05-03 11:22:47 +0200

    Update

--- Dice_rolling_simulator.py
+++ Dice_rolling_simulator.py
@@ -1,9 +1,14 @@
 import random
 
-rolling = True
-while rolling:
-    print("Your number", random.randrange(1,6))
-    print("Do you want to roll again?")
-    rolling = "yes" in input()
-print("Thanks for a game")
+
+def rolling():
+    rolling = True
+    while rolling:
+        print("Your number is ", random.randrange(1,6))
+        print("Do you want to roll again?")
+        rolling = ("yes" or "y") in input().lower()
+    print("Thanks for a game")
+
+
+rolling()
 
